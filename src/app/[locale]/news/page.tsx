fbfa7fc66d183901e0088bd817cfe332a9640d5c
SergWebAcceptance <serg.web@acceptance.ltd>
2025-07-29 15:28:24 +0300

    Update SEO metadata across multiple pages to enhance descriptions and titles for better search visibility and user engagement.

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -1,4 +1,18 @@
+import type { Metadata } from 'next';
+
 import { Hero, NewsRow, ReadyToHear } from './components';
+
+export const metadata: Metadata = {
+  title: 'AI Calling & Industry News',
+  description:
+    'Stay informed with the latest news and updates on AI calling, AI call center software, and AI voice agents. Explore updates on AI call solutions shaping how businesses handle calls.',
+  openGraph: {
+    title: 'AI Calling & Industry News',
+    description:
+      'Stay informed with the latest news and updates on AI calling, AI call center software, and AI voice agents. Explore updates on AI call solutions shaping how businesses handle calls.',
+    //images: 'https://coldi.ai/images/meta.png',
+  },
+};
 
 export default function News() {
   return (
--- src/app/[locale]/pricing/page.tsx
+++ src/app/[locale]/pricing/page.tsx
@@ -1,6 +1,20 @@
+import type { Metadata } from 'next';
+
 import { PricingTabs } from '@/features/pricing/ui/pricing-tabs/PricingTabs';
 
 import { Hero, Infrustructure, ReadyToHear } from './components';
+
+export const metadata: Metadata = {
+  title: 'AI Voice Agent & Call Center Solutions Pricing',
+  description:
+    'See AI voice agent pricing and plans for inbound calls, booking agents, and full-service voice solutions. Discover affordable AI call center software built to scale. Request your AI voice agent now!',
+  openGraph: {
+    title: 'AI Voice Agent & Call Center Solutions Pricing',
+    description:
+      'See AI voice agent pricing and plans for inbound calls, booking agents, and full-service voice solutions. Discover affordable AI call center software built to scale. Request your AI voice agent now!',
+    //images: 'https://coldi.ai/images/meta.png',
+  },
+};
 
 export default function Products() {
   return (
