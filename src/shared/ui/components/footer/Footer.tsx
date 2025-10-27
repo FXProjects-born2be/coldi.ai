6b9f23083139c44b0a5922e95a188bcaf29155a7
SergWebAcceptance <serg.web@acceptance.ltd>
2025-10-27 14:14:23 +0200

    Add fixed WhatsApp link to Footer component and enhance styles for improved visibility and interaction

--- src/shared/ui/components/footer/Footer.tsx
+++ src/shared/ui/components/footer/Footer.tsx
@@ -10,6 +10,7 @@ import { PhoneIcon } from '@/shared/ui/icons/fill/phone';
 import { Facebook } from '@/shared/ui/icons/fill/socials/facebook';
 import { Linkedin } from '@/shared/ui/icons/fill/socials/linkedin';
 import { Whatsapp } from '@/shared/ui/icons/fill/socials/whatsapp';
+import { WhatsappFixed } from '@/shared/ui/icons/fill/socials/whatsapp-fixed';
 import { X } from '@/shared/ui/icons/fill/socials/x';
 
 import st from './Footer.module.scss';
@@ -18,77 +19,82 @@ export const Footer = () => {
   const pathname = usePathname();
 
   return !requestRoutes.has(pathname) ? (
-    <footer className={st.footer}>
-      <div className={st.footer__container}>
-        <section className={st.footer__content}>
-          <div className={st.footer__logo}>
-            <Image src="/full-logo.svg" alt="Coldi" width={145} height={50} />
-            <p>Brand-Tuned Ai Talkers. Always On.</p>
-            <div className={st.footer__socials}>
-              <Link href="https://x.com/Coldiai" target="_blank">
-                <X />
-              </Link>
-              <Link href="https://www.facebook.com/coldiai/" target="_blank">
-                <Facebook />
-              </Link>
-              <Link href="https://www.linkedin.com/company/coldiai/" target="_blank">
-                <Linkedin />
-              </Link>
-              <Link href="https://wa.me/447521142301" target="_blank">
-                <Whatsapp />
-              </Link>
+    <>
+      <footer className={st.footer}>
+        <div className={st.footer__container}>
+          <section className={st.footer__content}>
+            <div className={st.footer__logo}>
+              <Image src="/full-logo.svg" alt="Coldi" width={145} height={50} />
+              <p>Brand-Tuned Ai Talkers. Always On.</p>
+              <div className={st.footer__socials}>
+                <Link href="https://x.com/Coldiai" target="_blank">
+                  <X />
+                </Link>
+                <Link href="https://www.facebook.com/coldiai/" target="_blank">
+                  <Facebook />
+                </Link>
+                <Link href="https://www.linkedin.com/company/coldiai/" target="_blank">
+                  <Linkedin />
+                </Link>
+                <Link href="https://wa.me/447521142301" target="_blank">
+                  <Whatsapp />
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className={st.footer__links}>
-            <ul
-              className={st.footer__list}
-              itemScope
-              itemType="http://schema.org/SiteNavigationElement"
-            >
-              <h4>Explore</h4>
-              <li itemProp="name">
-                <Link href="/products" itemProp="url">
-                  Products
-                </Link>
-              </li>
-              <li itemProp="name">
-                <Link href="/pricing" itemProp="url">
-                  Pricing
-                </Link>
-              </li>
-              <li itemProp="name">
-                <Link href="/news" itemProp="url">
-                  News
-                </Link>
-              </li>
-              <li itemProp="name">
-                <Link href="/about" itemProp="url">
-                  About
-                </Link>
-              </li>
-            </ul>
-            <ul className={st.footer__list}>
-              <h4>Contact Us</h4>
-              <li>
-                <Link href="mailto:info@coldi.ai">
-                  <MessageIcon />
-                  info@coldi.ai
-                </Link>
-              </li>
-              <li>
-                <Link href="tel:+441299667777">
-                  <PhoneIcon />
-                  +441299667777
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <span className={st.divider} />
-        <p className={st.footer__copyright}>
-          © {new Date().getFullYear()} Coldi. Voice-Powered. Rights Reserved.
-        </p>
-      </div>
-    </footer>
+            <div className={st.footer__links}>
+              <ul
+                className={st.footer__list}
+                itemScope
+                itemType="http://schema.org/SiteNavigationElement"
+              >
+                <h4>Explore</h4>
+                <li itemProp="name">
+                  <Link href="/products" itemProp="url">
+                    Products
+                  </Link>
+                </li>
+                <li itemProp="name">
+                  <Link href="/pricing" itemProp="url">
+                    Pricing
+                  </Link>
+                </li>
+                <li itemProp="name">
+                  <Link href="/news" itemProp="url">
+                    News
+                  </Link>
+                </li>
+                <li itemProp="name">
+                  <Link href="/about" itemProp="url">
+                    About
+                  </Link>
+                </li>
+              </ul>
+              <ul className={st.footer__list}>
+                <h4>Contact Us</h4>
+                <li>
+                  <Link href="mailto:info@coldi.ai">
+                    <MessageIcon />
+                    info@coldi.ai
+                  </Link>
+                </li>
+                <li>
+                  <Link href="tel:+441299667777">
+                    <PhoneIcon />
+                    +441299667777
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <span className={st.divider} />
+          <p className={st.footer__copyright}>
+            © {new Date().getFullYear()} Coldi. Voice-Powered. Rights Reserved.
+          </p>
+        </div>
+      </footer>
+      <Link href="https://wa.me/447521142301" target="_blank" className={st.whatsappFixedLink}>
+        <WhatsappFixed />
+      </Link>
+    </>
   ) : null;
 };
