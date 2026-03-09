be69148ada13a6b409ad864b18e58ba02f987ac5
SergWebAcceptance <serg.web@acceptance.ltd>
2026-03-09 17:08:06 +0200

    Refactor Hero component styles for improved layout and responsiveness; center title text and adjust padding for better mobile experience.

--- src/app/calendar/components/Hero/Hero.tsx
+++ src/app/calendar/components/Hero/Hero.tsx
@@ -27,23 +27,26 @@ export const Hero = () => {
   return (
     <section className={st.layout}>
       <div className={st.title}>
-        <div>
-          <h1>
-            <span>Your subscription</span> is <br />
-            not fully confirmed
-          </h1>
-          <p>Watch this video until the end (only 1 minute).</p>
-        </div>
-        <div className={st.video}>
-          <div
-            id="vidalytics_embed_7GATnpENZk_o6u6D"
-            style={{ width: '100%', position: 'relative', paddingTop: '56.25%' }}
-          />
-          <Script
-            id="vidalytics-embed"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
+        <h1>
+          <span>Your subscription</span> <br />
+          is not fully confirmed
+        </h1>
+        <p>Watch this video until the end (only 1 minute).</p>
+      </div>
+      <div className={st.video}>
+        <div
+          id="vidalytics_embed_7GATnpENZk_o6u6D"
+          style={{
+            width: '100%',
+            position: 'relative',
+            paddingTop: '56.25%',
+          }}
+        />
+        <Script
+          id="vidalytics-embed"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
 (function (v, i, d, a, l, y, t, c, s) {
 y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
 if (!vsl){vsl=function(u,cb){
@@ -54,9 +57,8 @@ i.getElementsByTagName("head")[0].appendChild(s);
 vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
 })(window, document, 'Vidalytics', 'vidalytics_embed_7GATnpENZk_o6u6D', 'https://fast.vidalytics.com/embeds/J3s4CyNK/7GATnpENZk_o6u6D/');
               `,
-            }}
-          />
-        </div>
+          }}
+        />
       </div>
       <div className={st.calendar}>
         <div className={`${st.meetingsEmbed} meetings-iframe-container`} data-src={embedUrl} />
