8d82e8554ebfa5faa996ddede86e54d079c03015
artem <artempasko880@gmail.com>
2026-09-03 11:26:08 +0300

    add about page

--- src/shared/ui/components/header/nav.ts
+++ src/shared/ui/components/header/nav.ts
@@ -3,6 +3,7 @@ export const headerIndustryItems = [
   { id: 'trading', href: '/industries/trading-platforms-brokers' },
   { id: 'debt-collection', href: '/industries/debt-collection' },
   { id: 'emis', href: '/industries/emis-payments' },
+  { id: 'other', href: '/industries#other-industries' },
 ] as const;
 
 export const headerAboutItems = [{ id: 'meet-the-team', href: '/meettheteam' }] as const;
